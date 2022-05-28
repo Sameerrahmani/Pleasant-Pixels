213a1cc90c25d02535d54287a11a6ad1cd133af4
sameerrahmani <56892116+Sameerrahmani@users.noreply.github.com>
2022-05-28 12:28:07 -0400

    Update GameScene.cs

--- GameScene.cs
+++ GameScene.cs
@@ -3,15 +3,29 @@ using System;
 
 public class GameScene : Node2D
 {
-	public Label OnOff;
-	public Label RainOnOff;
+	public Panel ControlPanel;
 	public AudioStreamPlayer2D AmbiancePlayer;
 	public AudioStreamPlayer2D MusicPlayer;
+	public Panel EscapePanel;
+	public RichTextLabel Time;
 	
 	public override void _Ready()
 	{
 		MusicPlayer = GetNode<AudioStreamPlayer2D>("Control/MusicPlayer");
 		AmbiancePlayer = GetNode<AudioStreamPlayer2D>("Control/AmbiancePlayer");
+		ControlPanel = GetNode<Panel>("Control/Panel");
+		EscapePanel = GetNode<Panel>("Control/Panel2");
+		Time = GetNode<RichTextLabel>("Control/Time");
+		
+		
+		
+		
+	}
+	
+	public override void _Process(float delta)
+	{
+		var time = OS.GetTime();
+		Time.Text = time.ToString();
 	}
 	
 
@@ -43,16 +57,32 @@ public class GameScene : Node2D
 	
 	private void _on_TextureButton_toggled(bool button_pressed)
 	{
+		if (button_pressed == true)
+		{
+			EscapePanel.RectPosition = new Vector2(913, 1028);
+			ControlPanel.Visible = false;
+		}
 		
+		else if (button_pressed == false)
+		{
+			EscapePanel.RectPosition = new Vector2(913, 740);
+			ControlPanel.Visible = true;
+		}
+	}
+	
+	private void _on_ClockButton_toggled(bool button_pressed)
+	{
+		if (button_pressed == true)
+		{
+			Time.Visible = true;
+		}
+		
+		else if (button_pressed == false)
+		{
+			Time.Visible = false;
+		}
 	}
 }
-
-
-
-
-
-
-
 
 
 
