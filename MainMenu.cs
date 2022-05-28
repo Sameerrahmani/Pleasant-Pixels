725ac735490dd9888c5f667eb3fe175d22b16abe
sameerrahmani <56892116+Sameerrahmani@users.noreply.github.com>
2022-05-28 11:45:14 -0400

    Create MainMenu.cs

---
+++ MainMenu.cs
@@ -0,0 +1,31 @@
+using Godot;
+using System;
+
+public class MainMenu : Control
+{
+	public Button StartButton;
+	public AnimationPlayer DayNight;
+	
+	public override void _Ready()
+	{
+		StartButton = GetNode<Button>("CanvasLayer/StartButton");
+		DayNight = GetNode<AnimationPlayer>("CanvasModulate/AnimationPlayer");
+		
+		DayNight.Play("DayNightCycle");
+		
+	}
+
+	private void _on_StartButton_pressed()
+	{
+		GetTree().ChangeScene("res://Scenes/GameScene.tscn");
+	}
+
+	private void _on_ExitButton_pressed()
+	{
+		GetTree().Quit();
+	}
+}
+
+
+
+
