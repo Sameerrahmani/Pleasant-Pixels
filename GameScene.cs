b8d183d3c80f4bdd0420dfd4a66296548eb0ffeb
sameerrahmani <56892116+Sameerrahmani@users.noreply.github.com>
2022-05-28 21:43:58 -0400

    Update GameScene.cs

--- GameScene.cs
+++ GameScene.cs
@@ -5,40 +5,39 @@ public class GameScene : Node2D
 {
 	public Panel ControlPanel;
 	public AudioStreamPlayer2D AmbiancePlayer;
-	public AudioStreamPlayer2D MusicPlayer;
+	public AudioStreamPlayer2D MusicPlayer1;
 	public Panel EscapePanel;
-	public RichTextLabel Time;
+	public Panel Clock;
+	public AnimatedSprite AnimatedScene1;
+	public AnimatedSprite AnimatedScene2;
+	public Node2D Space;
+	
+
 	
 	public override void _Ready()
 	{
-		MusicPlayer = GetNode<AudioStreamPlayer2D>("Control/MusicPlayer");
+		MusicPlayer1 = GetNode<AudioStreamPlayer2D>("Control/MusicPlayer1");
 		AmbiancePlayer = GetNode<AudioStreamPlayer2D>("Control/AmbiancePlayer");
 		ControlPanel = GetNode<Panel>("Control/Panel");
 		EscapePanel = GetNode<Panel>("Control/Panel2");
-		Time = GetNode<RichTextLabel>("Control/Time");
-		
-		
-		
+		Clock = GetNode<Panel>("Control/Panel3");
+		AnimatedScene1 = GetNode<AnimatedSprite>("Control/AnimatedScene1");
+		AnimatedScene2 = GetNode<AnimatedSprite>("Control/AnimatedScene2");
+		Space = GetNode<Node2D>("Space");
 		
 	}
-	
-	public override void _Process(float delta)
-	{
-		var time = OS.GetTime();
-		Time.Text = time.ToString();
-	}
-	
+		
 
 	private void _on_MusicButton_toggled(bool button_pressed)
 	{
 		if (button_pressed == true)
 		{
-			MusicPlayer.Playing = true;
+			MusicPlayer1.Playing = true;
 		}
 		
 		else if (button_pressed == false)
 		{
-			MusicPlayer.Playing = false;
+			MusicPlayer1.Playing = false;
 		}
 	}
 	
@@ -65,7 +64,7 @@ public class GameScene : Node2D
 		
 		else if (button_pressed == false)
 		{
-			EscapePanel.RectPosition = new Vector2(913, 740);
+			EscapePanel.RectPosition = new Vector2(913, 834);
 			ControlPanel.Visible = true;
 		}
 	}
@@ -74,15 +73,108 @@ public class GameScene : Node2D
 	{
 		if (button_pressed == true)
 		{
-			Time.Visible = true;
+			Clock.Visible = true;
 		}
 		
 		else if (button_pressed == false)
 		{
-			Time.Visible = false;
+			Clock.Visible = false;
 		}
 	}
+	
+	private void _on_RainyBedroom_pressed()
+	{
+		AnimatedScene1.Visible = true;
+		if (AnimatedScene2.Visible == true)
+		{
+			AnimatedScene2.Visible = false;
+		}
+		
+		else if (Space.Visible == true)
+		{
+			Space.Visible = false;
+		}
+	}
+	
+	private void _on_RainyLake_pressed()
+	{
+		AnimatedScene2.Visible = true;
+		if (AnimatedScene1.Visible == true)
+		{
+			AnimatedScene1.Visible = false;
+		}
+		
+		else if (Space.Visible == true)
+		{
+			Space.Visible = false;
+		}
+	}
+	
+	private void _on_Space_pressed()
+	{
+		Space.Visible = true;
+		if (AnimatedScene1.Visible == true)
+		{
+			AnimatedScene1.Visible = false;
+		}
+		
+		else if (AnimatedScene2.Visible == true)
+		{
+			AnimatedScene2.Visible = false;
+		}
+	}
+	
+	private void _on_Lofi_pressed()
+	{
+		var Lofi = ResourceLoader.Load("res://Assets/Lofi.mp3") as AudioStream;
+			
+		MusicPlayer1.Stream = Lofi;
+		MusicPlayer1.Play();
+	}
+	
+	private void _on_Fantasy_pressed()
+	{
+		var Fantasy = ResourceLoader.Load("res://Assets/Now-We-Ride.mp3") as AudioStream;
+			
+		MusicPlayer1.Stream = Fantasy;
+		MusicPlayer1.Play();
+	}
+	
+	private void _on_Piano_pressed()
+	{
+		var Piano = ResourceLoader.Load("res://Assets/a-promise.mp3") as AudioStream;
+		
+		MusicPlayer1.Stream = Piano;
+		MusicPlayer1.Play();
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
